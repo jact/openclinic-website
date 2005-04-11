e8d51ac31a6b3f755a0739e6028d7f64b4dc7126
Jose Antonio Chavarría <jachavar@gmail.com>
2005-04-11 18:03:19 +0000

    Se cambia la dirección de correo del proyecto. Se añaden varias noticias: cambio de dirección de correo, lanzamiento de language packs

--- news.php
+++ news.php
@@ -6,7 +6,7 @@
  * Licensed under the Creative Commons License.
  * For full terms see the URL http://creativecommons.org/licenses/by/2.0/.
  *
- * $Id: news.php,v 1.1 2005/02/22 19:14:30 jact Exp $
+ * $Id: news.php,v 1.2 2005/04/11 18:03:19 jact Exp $
  */
 
 /**
@@ -14,7 +14,7 @@
  ********************************************************************
  * News page
  ********************************************************************
- * Author: jact <jachavar@terra.es>
+ * Author: jact <openclinic@gmail.com>
  */
 
   ////////////////////////////////////////////////////////////////////
@@ -28,6 +28,34 @@
   $title = "News";
   require_once("./header.php");
 ?>
+
+<div class="notice">
+  <h1>Cambio de la dirección de correo del proyecto</h1>
+
+  <p>
+    Con objeto de conseguir una cuenta de correo con más espacio y mejor eficiencia en los envíos (hemos tenido muchos problemas en este aspecto con la dirección actual), la nueva cuenta para contactar con nosotros es <strong>&lt;openclinic at gmail dot com&gt;</strong>. Os rogamos que ya no escribais a la anterior. Demasiados problemas nos ha proporcionado hasta ahora.
+  </p>
+
+  <p>
+    Y una recomendación más: para evitar confundir vuestros mensajes con el spam que acosará a esta nueva cuenta, os agradeceríamos que como Subject del correo siempre apareciera la palabra <q><strong>OpenClinic</strong></q>. Muchas gracias por vuestra colaboración.
+  </p>
+
+  <p class="sign">2005-04-10</p>
+</div>
+
+<div class="notice">
+  <h1>New language packs released</h1>
+
+  <p>
+    Dirk De Naegel ha traducido las versiones 0.6 y 0.7 de OpenClinic al holandés (nl_BE). Eric Tang se ha encargado de traducir al chino tradicional (zh_TW BIG5) la versión 0.7. Muchas gracias a los dos. Todas ellas están disponibles en la <a href="http://sourceforge.net/project/showfiles.php?group_id=70742">SourceForge download page</a> de OpenClinic.
+  </p>
+
+  <p>
+    Os recordamos que podeis contribuir aportando nuevas traducciones. Aunque también de otras formas. En el apartado <a href="./participate.php">Participate</a> podeis consultar algunas de ellas.
+  </p>
+
+  <p class="sign">2005-04-10</p>
+</div>
 
 <div class="notice">
   <h1>OpenClinic 0.7 released!</h1>
