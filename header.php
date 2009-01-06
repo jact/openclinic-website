6e86fd2137119de117a57f02678978ab019690b4
Jose Antonio Chavarría <jachavar@gmail.com>
2009-01-06 15:05:33 +0000

    Cambios en la cabecera del archivo Se añade la cabecera icon Se cambia la dirección del feed Cambios en la estructura de las páginas (más semántica y más escalabilidad)

--- header.php
+++ header.php
@@ -1,20 +1,16 @@
 <?php
 /**
- * This file is part of OpenClinic Web Site
- *
- * Copyright (c) 2002-2006 jact
- * Licensed under the Creative Commons License.
- * For full terms see the URL http://creativecommons.org/licenses/by/2.0/.
- *
- * $Id: header.php,v 1.6 2006/02/12 18:02:56 jact Exp $
- */
-
-/**
  * header.php
  *
  * Contains the common header of the web pages
  *
- * Author: jact <openclinic@gmail.com>
+ * Licensed under the Creative Commons License
+ *
+ * @package   OpenClinic Web Site
+ * @copyright 2002-2009 jact
+ * @license   http://creativecommons.org/licenses/by/2.0/
+ * @version   CVS: $Id: header.php,v 1.7 2009/01/06 15:05:33 David Bowman Exp $
+ * @author    jact <openclinic@gmail.com>
  */
 
   if (str_replace("\\", "/", __FILE__) == $_SERVER['SCRIPT_FILENAME'])
@@ -23,6 +19,7 @@
     exit();
   }
 
+  //error_reporting(E_ALL ^ E_NOTICE); // @debug
   /**
    * XHTML Start (XML prolog, DOCTYPE, title page and meta data)
    */
@@ -33,29 +30,33 @@
 
 <link href="mailto:openclinic&#64;gmail.com" rev="made" />
 
+<link rel="icon" type="image/png" href="./i/miniopc.png" />
+
 <link rel="shortcut icon" type="image/png" href="./i/miniopc.png" />
 
 <link rel="bookmark icon" type="image/png" href="./i/miniopc.png" />
 
 <link href="./c/openclinic.css" type="text/css" rel="stylesheet" title="OpenClinic Web Site" />
 
-<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="./ocw_rss2.php" />
+<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="./feed.php" />
 </head>
 <body>
-<div id="heading">
-  <p id="homePage">
-    <a href="./index.php" title="Home page"><img id="logo" src="./i/openclinic_logo.png" width="291" height="58" alt="Home page" /></a>
-  </p>
+<div id="wrap">
+  <div id="header">
+    <p id="home_page">
+      <a href="./index.php" title="Home page"><img id="logo" src="./i/openclinic_logo.png" width="291" height="58" alt="Home page" /></a>
+    </p>
 
-  <ul id="shortcuts">
-    <li><a href="http://sourceforge.net/project/showfiles.php?group_id=70742" title="Shortcut to SourceForge download page"><img src="./i/dumps.png" width="36" height="36" title="Download" alt="Download" />Download</a></li>
+    <ul id="shortcuts">
+      <li><a href="http://sourceforge.net/project/showfiles.php?group_id=70742" title="Shortcut to SourceForge download page"><img src="./i/download.png" width="48" height="48" title="Download" alt="Download" />Download</a></li>
 
-    <li><a href="./openclinic/index.php" title="Shortcut to demo version"><img src="./i/users.png" width="36" height="36" title="Demo" alt="Demo" />Demo</a></li>
+      <li><a href="./openclinic/index.php" title="Shortcut to demo version"><img src="./i/demo.png" width="48" height="48" title="Demo" alt="Demo" />Demo</a></li>
 
-    <li><a href="./faq.php"><img src="./i/logs.png" width="36" height="36" title="FAQ" alt="FAQ" />FAQ</a></li>
-  </ul>
-</div>
+      <li><a href="./faq.php"><img src="./i/faq.png" width="48" height="48" title="FAQ" alt="FAQ" />FAQ</a></li>
+    </ul>
+  </div>
 
-<hr />
+  <hr id="separator" />
 
-<div id="content">
+  <div id="main" class="line">
+    <div id="content" class="item">
