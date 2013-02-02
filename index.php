a45afcbe04ec99dbd996f1a277dae5641b9cc19a
Jose Antonio Chavarría <jachavar@gmail.com>
2013-02-02 20:02:53 +0000

    Se actualizan requisitos
    Se elimina el búlgaro de la lista de traducciones

--- index.php
+++ index.php
@@ -7,9 +7,9 @@
  * Licensed under the Creative Commons License
  *
  * @package   OpenClinic Web Site
- * @copyright 2002-2009 jact
+ * @copyright 2002-2013 jact
  * @license   http://creativecommons.org/licenses/by/2.0/
- * @version   CVS: $Id: index.php,v 1.5 2009/01/10 14:36:17 David Bowman Exp $
+ * @version   CVS: $Id: index.php,v 1.6 2013/02/02 20:02:53 David Bowman Exp $
  * @author    jact <openclinic@gmail.com>
  */
 
@@ -71,7 +71,7 @@
 
         <li>System Users</li>
 
-        <li>Dumps</li>
+        <li>Database Dumps</li>
 
         <li>Log Statistics</li>
       </ul>
@@ -81,13 +81,33 @@
   <h3>Requirements</h3>
 
   <ul>
-    <li><a href="http://www.php.net/">PHP4</a> &gt;= 4.2 (due to session variable changes in PHP 4.2) compiled with MySQL support (tested with versions 4.2.x, 4.3.x and 4.4.x);</li>
+    <li>
+      <p>For OpenClinic &lt;= 0.7:</p>
 
-    <li><a href="http://www.mysql.com/">MySQL</a> (tested with versions 3.23.36, 3.23.49, 3.23.51, 3.23.52, 3.23.54, 3.23.55, 4.0.16, 4.0.18 and 4.0.26);</li>
+      <ul>
+        <li><a href="http://www.php.net/">PHP4</a> &gt;= 4.2 (due to session variable changes in PHP 4.2) compiled with MySQL support (tested with versions 4.2.x, 4.3.x and 4.4.x);</li>
 
-    <li>a web server prepared to execute <acronym title="PHP: Hypertext Preprocessor">PHP</acronym> code (like <a href="http://httpd.apache.org/">Apache</a>);</li>
+        <li><a href="http://www.mysql.com/">MySQL</a> (tested with versions 3.23.36, 3.23.49, 3.23.51, 3.23.52, 3.23.54, 3.23.55, 4.0.16, 4.0.18 and 4.0.26)</li>
+      </ul>
+    </li>
 
-    <li>and, of course, a web browser (with JavaScript and good <acronym title="Cascading Style Sheet level 2">CSS2</acronym> support). We recommend <a href="http://www.opera.com/">Opera</a> or <a href="http://www.mozilla.org/">Firefox</a>.</li>
+    <li>
+      <p>For OpenClinic &gt; 0.7:</p>
+
+      <ul>
+        <li>PHP5 &gt;= 5.3.1 (incompatible with PHP4) <strong>compiled with MySQL support</strong>;</li>
+
+        <li>MySQL &gt; 5 (tested with 5.1 version)</li>
+      </ul>
+    </li>
+  </ul>
+
+  <p>Common requirements for all versions:</p>
+
+  <ul>
+    <li>A web server prepared to execute <acronym title="PHP: Hypertext Preprocessor">PHP</acronym> code (like <a href="http://httpd.apache.org/">Apache</a>);</li>
+
+    <li>and, of course, a web browser (with JavaScript and good <acronym title="Cascading Style Sheet level 2">CSS2</acronym> support). We recommend <a href="http://www.opera.com/">Opera</a>, <a href="http://www.mozilla.org/">Firefox</a> or <a href="http://download-chromium.appspot.com">Chromium</a>.</li>
   </ul>
 
   <h3>Features</h3>
@@ -103,7 +123,7 @@
     <dd><a href="./openclinic/LICENSE">GNU General Public License (GPL)</a></dd>
 
     <dt>Language</dt>
-    <dd>English (all versions), Spanish (all versions), Bulgarian (from 0.3 to 0.5 version, both included), Dutch (0.6 and 0.7 versions), Traditional Chinese (0.7 version)</dd>
+    <dd>English (all versions), Spanish (all versions), Bulgarian (from 0.3 to 0.5 version, both included), Dutch (0.6, 0.7 and 0.8 versions), Traditional Chinese (0.7 and 0.8 versions)</dd>
 
     <dt>Operating System</dt>
     <dd>OS Independent</dd>
