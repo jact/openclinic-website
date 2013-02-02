6e7d790113bb8188a488edec39d04d81aa48b975
Jose Antonio Chavarría <jachavar@gmail.com>
2013-02-02 20:02:00 +0000

    Si se usa PHP >= 5 se establece la zona horaria
    Se añade como noticia el release de la versión 0.8

--- feed.php
+++ feed.php
@@ -7,9 +7,9 @@
  * Licensed under the Creative Commons License
  *
  * @package   OpenClinic Web Site
- * @copyright 2002-2009 jact
+ * @copyright 2002-2013 jact
  * @license   http://creativecommons.org/licenses/by/2.0/
- * @version   CVS: $Id: feed.php,v 1.1 2009/01/06 15:07:06 David Bowman Exp $
+ * @version   CVS: $Id: feed.php,v 1.2 2013/02/02 20:02:00 David Bowman Exp $
  * @author    jact <openclinic@gmail.com>
  */
 
@@ -18,7 +18,12 @@
  * string appendNotices(array &$notices, string $category, string $page)
  */
 
-  $author = utf8_encode('Jose Antonio ChavarrÌa');
+  if (version_compare(phpversion(), '5.0.0') > 0)
+  {
+    date_default_timezone_set('Europe/Madrid');
+  }
+
+  $author = /*utf8_encode*/('Jose Antonio Chavarr√≠a');
   $email = 'openclinic@gmail.com';
   $format = 'D, j M Y H:i:s O';
   $host = 'http://openclinic.sourceforge.net';
@@ -34,7 +39,7 @@
   $feed .= 'OpenClinic is an easy to use, open source, medical records system written in PHP. It has been mainly thougth for private clinics, surgeries and private doctors. It is platform independent and it has multilanguage architecture.';
   $feed .= '</description>';
 
-  $feed .= '<pubDate>' . date($format, mktime(20, 21, 3, 4, 11, 2005)) . '</pubDate>'; // h, i, s, m, d, y
+  $feed .= '<pubDate>' . date($format, mktime(20, 46, 10, 2, 2, 2013)) . '</pubDate>'; // h, i, s, m, d, y
   $feed .= '<generator>' . 'OpenClinic Web Site' . '</generator>';
   $feed .= '<language>' . 'en' . '</language>';
   $feed .= '<webMaster>' . $email . ' (' . $author . ')' . '</webMaster>';
@@ -44,22 +49,56 @@
   $feed .= '<ttl>' . '60' . '</ttl>';
 
   $news = array( // guid_fragment => array(title, pubDate)
-    'email_change' => array('New mail address project', date($format, mktime(20, 1, 3, 4, 10, 2005))),
-    'new_language_packs' => array('New language packs released', date($format, mktime(21, 1, 3, 4, 10, 2005))),
-    'released_0_7' => array('OpenClinic 0.7 released!', date($format, mktime(23, 31, 32, 12, 7, 2004))),
-    'upgrade_0_6' => array('Upgrade from 0.6 version', date($format, mktime(23, 11, 45, 12, 7, 2004)))
+    'released_0_8' => array(
+      'OpenClinic 0.8 released after 8 years!',
+      date($format, mktime(20, 46, 10, 2, 2, 2013))
+    ),
+    'email_change' => array(
+      'New mail address project',
+      date($format, mktime(20, 1, 3, 4, 10, 2005))
+    ),
+    'new_language_packs' => array(
+      'New language packs released',
+      date($format, mktime(21, 1, 3, 4, 10, 2005))
+    ),
+    'released_0_7' => array(
+      'OpenClinic 0.7 released!',
+      date($format, mktime(23, 31, 32, 12, 7, 2004))
+    ),
+    'upgrade_0_6' => array(
+      'Upgrade from 0.6 version',
+      date($format, mktime(23, 11, 45, 12, 7, 2004))
+    ),
   );
 
   $feed .= '<!-- News category -->';
   $feed .= appendNotices($news, 'News', 'news.php');
 
   $faq = array( // guid_fragment => array(title, pubDate)
-    'deleting_patient_problem' => array('Deleting a medical problem or patient bug', date($format, mktime(21, 19, 10, 1, 16, 2006))),
-    'medical_tests_problem' => array('Problem to see medical tests', date($format, mktime(20, 1, 3, 4, 10, 2005))),
-    'translate_date_entries' => array('How to translate the date-relative entries', date($format, mktime(20, 15, 52, 4, 6, 2005))),
-    'staff_member_header' => array('How to fix the Staff Member bug in 0.7 version', date($format, mktime(19, 57, 25, 4, 6, 2005))),
-    'second_surname_not_compulsory' => array('How to achieve that the second surname family name was not compulsory', date($format, mktime(21, 3, 14, 4, 4, 2005))),
-    'install_authentication' => array('Authentication dialogue to get access to install folder', date($format, mktime(19, 22, 56, 4, 4, 2005)))
+    'deleting_patient_problem' => array(
+      'Deleting a medical problem or patient bug',
+      date($format, mktime(21, 19, 10, 1, 16, 2006))
+    ),
+    'medical_tests_problem' => array(
+      'Problem to see medical tests',
+      date($format, mktime(20, 1, 3, 4, 10, 2005))
+    ),
+    'translate_date_entries' => array(
+      'How to translate the date-relative entries',
+      date($format, mktime(20, 15, 52, 4, 6, 2005))
+    ),
+    'staff_member_header' => array(
+      'How to fix the Staff Member bug in 0.7 version',
+      date($format, mktime(19, 57, 25, 4, 6, 2005))
+    ),
+    'second_surname_not_compulsory' => array(
+      'How to achieve that the second surname family name was not compulsory',
+      date($format, mktime(21, 3, 14, 4, 4, 2005))
+    ),
+    'install_authentication' => array(
+      'Authentication dialogue to get access to install folder',
+      date($format, mktime(19, 22, 56, 4, 4, 2005))
+    ),
   );
 
   $feed .= '<!-- FAQ category -->';
